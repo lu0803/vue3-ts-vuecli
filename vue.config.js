56437ad12ee62af9fa353d1925caa1bf83832bd3
lu0803 <78739374@qq.com>
2022-02-23 23:41:50 +0800

    feat(request): axios change
    improve axios packaging

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,7 @@
 const path = require('path')
-const AutoImport = require('unplugin-auto-import/webpack')
-const Components = require('unplugin-vue-components/webpack')
-const {
-  ElementPlusResolver
-} = require('unplugin-vue-components/resolvers')
+// const AutoImport = require('unplugin-auto-import/webpack')
+// const Components = require('unplugin-vue-components/webpack')
+// const { ElementPlusResolver } = require('unplugin-vue-components/resolvers')
 
 module.exports = {
   devServer: {
